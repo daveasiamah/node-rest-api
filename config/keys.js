83bef65c331a4cfc65b266fd5991db002960b310
David Asiamah <35697497+daveasiamah@users.noreply.github.com>
2020-07-24 05:19:06 +0100

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // mongodbURI: "mongodb://dave:dave123@ds249873.mlab.com:49873/ims-pos"
-  // mongodbURI: "mongodb://localhost:27017/ims-pos2",
-  mongodbURI:"mongodb+srv://david:9iaR73McI4aercan@cluster0.kizxu.gcp.mongodb.net/eps-imsdb?retryWrites=true&w=majority",
-  secretOrkey: "MIIALL99288155#@{22!#$$CCc00iaEchaE**",
-  baseServerUri: process.env.BASE_SERVER_URI ||'https://eps-ims-rest-api'
-};
