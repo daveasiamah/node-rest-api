b7bc798ed181e41cf93d32f09c924696834b5ea7
david <daveasiamah@gmail.com>
2020-07-24 03:55:41 +0000

    using  mongodb cloud

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,7 @@
 module.exports = {
   // mongodbURI: "mongodb://dave:dave123@ds249873.mlab.com:49873/ims-pos"
-  mongodbURI: "mongodb://localhost:27017/ims-pos2",
-  secretOrkey: "secretKey",
-  baseServerUri: "http://localhost:7000"
+  // mongodbURI: "mongodb://localhost:27017/ims-pos2",
+  mongodbURI:"mongodb+srv://david:9iaR73McI4aercan@cluster0.kizxu.gcp.mongodb.net/eps-imsdb?retryWrites=true&w=majority",
+  secretOrkey: "MIIALL99288155#@{22!#$$CCc00iaEchaE**",
+  baseServerUri: process.env.BASE_SERVER_URI ||'https://eps-ims-rest-api'
 };
