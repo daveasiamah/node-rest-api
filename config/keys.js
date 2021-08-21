06a40309188e766f0a7bdb710cb21a3ab96785bb
daveasiamah7 <daveasiamah7@gmail.com>
2021-08-21 11:48:51 +0000

    fixed db connection

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // mongodbURI: "mongodb://dave:dave123@ds249873.mlab.com:49873/ims-pos"
+  // mongodbURI: "mongodb://localhost:27017/ims-pos2",
+  mongodbURI:
+    "mongodb+srv://david:4C7VJevFFt1kOddi@cluster0.f5vcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrkey: "MIIALL99288155#@{22!#$$CCc00iaEchaE**",
+  baseServerUri:
+    process.env.BASE_SERVER_URI || "https://eps-ims-rest-api.herokuapp.com",
+};
